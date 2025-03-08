ed72e40fa7fa6289dfb82011491c708a14c8a361
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2025-03-08 14:11:37 +0530

    removed comments

--- app.js
+++ app.js
@@ -1,55 +1,39 @@
 const fs = require('fs');
 const net = require('net');
 
-const filePath = 'D:\\test.txt';  // Change to your file path
-const templatePath = 'testhtml';  // Change to your file path
-const SERVER_HOST = '127.0.0.1';  // Change to your server IP if needed
-const SERVER_PORT = 5250;         // Make sure this matches your server port
+const filePath = 'D:\\test.txt'; 
+const templatePath = 'testhtml';  
+const SERVER_HOST = '127.0.0.1'; 
+const SERVER_PORT = 5250;        
 
-// Create a TCP client and connect to the server
 const client = new net.Socket();
 client.connect(SERVER_PORT, SERVER_HOST, () => {
     console.log('Connected to server, sending initial message...');
-
-
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading file:', err.message);
             return;
         }
-
         let xml = '';
-             xml += `<componentData id=\\"${'ccgf0'}\\"><data id=\\"text\\" value=\\"${data}\\" /></componentData>`
-              xml = `"<templateData>${xml}</templateData>"`
-
-        // Send file content over TCP
-        // client.write(`cg 1-2 update 2 ${xml}\r\n`);
-    client.write(`cg 1-2 add 2 "${templatePath}" 1 ${xml} \r\n`);
-
+        xml += `<componentData id=\\"${'ccgf0'}\\"><data id=\\"text\\" value=\\"${data}\\" /></componentData>`
+        xml = `"<templateData>${xml}</templateData>"`
+        client.write(`cg 1-2 add 2 "${templatePath}" 1 ${xml} \r\n`);
     });
-
 });
-
-// Handle TCP errors
 client.on('error', (err) => {
     console.error('TCP Connection Error:', err.message);
 });
 
-// Watch for file changes
 fs.watch(filePath, (eventType, filename) => {
     if (filename) {
-        // Read the content of the file
         fs.readFile(filePath, 'utf8', (err, data) => {
             if (err) {
                 console.error('Error reading file:', err.message);
                 return;
             }
-
             let xml = '';
-                 xml += `<componentData id=\\"${'ccgf0'}\\"><data id=\\"text\\" value=\\"${data}\\" /></componentData>`
-                  xml = `"<templateData>${xml}</templateData>"`
-
-            // Send file content over TCP
+            xml += `<componentData id=\\"${'ccgf0'}\\"><data id=\\"text\\" value=\\"${data}\\" /></componentData>`
+            xml = `"<templateData>${xml}</templateData>"`
             client.write(`cg 1-2 update 2 ${xml}\r\n`);
         });
     }
